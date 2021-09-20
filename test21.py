736a9628806698e0eab2b213cb138251fc49fc42
Marcela Rangel López <mrangell75@gmail.com>
2021-09-20 10:16:48 -0500

    Update test21.py

--- test21.py
+++ test21.py
@@ -1,62 +1,67 @@
 import os, easygui
 import xml.etree.ElementTree as ET
-import pandas as pd
 
 import tkinter as tk
-from tkinter import *  # ttk, Button, Message
+from tkinter import *
+
+import pandas as pd
 
 LARGE_FONT = ("Verdana", 12)
 
+xml_name = ''
+tc_xml = []
 
 class AppMain(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, 'Analysis of a XML')
 
-        tk.Tk.wm_title(self, "Graphical XML file analysis")
-        tk.Tk.geometry(self, "1280x1024")
+        self.shared_xml = tk.StringVar()  ###### ***
 
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+        container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        # container.config(cursor="heart", relief="sunken")
 
+        self.shared_df = {'TC': tk.StringVar()}
         self.frames = {}
 
-        for F in (StartWindow, WindowOne, WindowTwo):
+        for F in (Home, PageOne, PageTwo):
             frame = F(container, self)
-
             self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
 
-            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Home)
 
-        self.show_frame(StartWindow)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+    def show_frame(self, container):
+        frame = self.frames[container]
         frame.tkraise()
 
 
-class StartWindow(tk.Frame):
+class Home(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Press the button to open the XML file:", font=LARGE_FONT)
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text='File Upload: Press the button to open the XML file.', font=LARGE_FONT)
         label.place(relx=.01, rely=.01)
 
+        # Function to display buttons and choose the TestCase to be plotted
         def separate_tc(xml_name):
+            global tc_xml
             file_xml = ET.parse(xml_name)
             tc_xml = [
                 {"Name": signal.attrib["Name"],
                  "Id": signal.attrib["Id"],
                  } for signal in file_xml.findall(".//Child_4")
             ]
-            print(tc_xml)
-            # WindowOne.select_tc(self, tc_xml)
+            controller.shared_xml.set(tc_xml)   ###### *** Assignment of values to variable shared_xml
+            print(controller.shared_xml.get())
+            controller.show_frame(PageOne)
 
-        # Functions to open pxml file
+        # Functions to open xml file
         def open_file():
             global xml_name
-            # if pxml_name == '':
             try:
                 xml_name = str(easygui.fileopenbox(title='Select XML file', default='*.xml'))
                 if str(os.path.abspath(xml_name)) != os.path.join(os.path.abspath(os.getcwd()),
@@ -68,40 +73,50 @@ class StartWindow(tk.Frame):
                 print('XML file was not loaded.')
 
         button_open = Button(self, text="Open File XML", command=open_file)
-        button_open.place(relx=.01, rely=.04)
+        button_open.place(relx=.01, rely=.03)
 
 
-class WindowOne(tk.Frame):
-
+class PageOne(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Select the TestCase:", font=LARGE_FONT)
-        label.pack(pady=5, padx=5)
+        super().__init__(parent)
+        self.controller = controller
 
-        def select_tc(self, tc_xml):
-            for i in tc_xml:
-                id_tc = i.get('Id')
-                dict_tc = str(i.values()).replace('dict_values([\'', '')
-                with_apo = dict_tc.replace('\'])', '')
-                name_tc = with_apo.replace("'", "")
-                buttons_tc = Button(self, text=f"TC> {name_tc}", command=lambda: controller.show_frame(WindowTwo))
-                buttons_tc.pack(pady=3)
+        label = tk.Label(self, text='Select the TestCase you want to plot:', font=LARGE_FONT)
+        label.place(relx=.01, rely=.01)
 
-        button_back = Button(self, text="Back to Home", command=lambda: controller.show_frame(StartWindow))
-        button_back.pack()
+        controller.shared_xml.get()  ###### *** How do I get the variable tc_xml from the Home class? ***
+        print(controller.shared_xml.get())
+
+        for i in tc_xml:
+            id_tc = i.get('Id')
+            dict_tc = str(i.values()).replace('dict_values([\'', '')
+            with_apo = dict_tc.replace('\'])', '')
+            name_tc = with_apo.replace("'", "")
+            buttons_tc = Button(self, text=f"TC> {name_tc}", command=PageTwo)
+            buttons_tc.pack(pady=3)
+
+        button_back = Button(self, text='Back to Home', command=lambda: controller.show_frame(Home))
+        button_back.place(relx=.01, rely=.03)
 
 
-class WindowTwo(tk.Frame):
+
+class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
+        self.controller = controller
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Child4 Page!", font=LARGE_FONT)
+        label = tk.Label(self, text='Page One!!!', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button_back = Button(self, text="Select Another Child4",
-                         command=lambda: controller.show_frame(WindowOne))
+        print("File to graph" + xml_name)
+        # self.shared_df = pd.DataFrame(xml_name)
+
+        button_one = Button(self, text='Back to Page One', command=lambda: controller.show_frame(PageOne))
+        button_one.pack()
+        button_back =Button(self, text='Back to Home', command=lambda: controller.show_frame(StartPage))
         button_back.pack()
 
 
 if __name__ == "__main__":
-    app = AppMain()
+    app = AppMain(None)
+    app.geometry("1280x1024")
     app.mainloop()
